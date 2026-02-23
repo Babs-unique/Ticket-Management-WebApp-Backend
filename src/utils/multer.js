8a9c43b8a1b0382ef81f49a4ca0ba41d7ddba016
Babs-unique <145204996+Babs-unique@users.noreply.github.com>
2026-02-24 00:52:50 +0100

    Made changes to the multer template  configuration for better functionality

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -3,17 +3,25 @@ const path = require('path');
 
 
 const storage = multer.diskStorage({
-    file : (req, file, cb) => {
-        const allowedExtensions = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp', '.tiff', '.svg','.mp4', '.mov', '.avi', '.pdf', '.docx'];
-        const ext = path.extname(file.originalname);
-
-        if(allowedExtensions.includes(ext.toLowerCase())) {
-            cb(null, true);
-        }else{
-            cb(new Error('File type not allowed'), false);
-        }
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
     }
-})
-const upload = multer({ storage});
+});
+
+const fileFilter = (req, file, cb) => {
+    const allowedExtensions = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp', '.tiff', '.svg','.mp4', '.mov', '.avi', '.pdf', '.docx'];
+    const ext = path.extname(file.originalname);
+
+    if(allowedExtensions.includes(ext.toLowerCase())) {
+        cb(null, true);
+    }else{
+        cb(new Error('File type not allowed'), false);
+    }
+};
+
+const upload = multer({ storage, fileFilter });
 
 module.exports = upload;
