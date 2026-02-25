53a6d4e99a2fec095c579a8b408dfc656a3234fe
Babs-unique <145204996+Babs-unique@users.noreply.github.com>
2026-02-25 18:16:05 +0100

    Implementation of errorhandler

--- app.js
+++ app.js
@@ -5,41 +5,40 @@ const morgan = require('morgan');
 const connectDB = require("./src/config/db");
 const userRoutes = require('./src/routes/user.routes');
 const ticketRoutes = require('./src/routes/ticket.routes');
-const settingRoutes = require('./src/routes/setting.routes')
+const settingRoutes = require('./src/routes/setting.routes');
+const errorHandler = require('./src/middleware/errorhandler');
 
 
 require('dotenv').config();
 
 
-
-
 const app = express();
-
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-
-
 connectDB();
 
+// Rate limiter
+const rateLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 50,
+    message: "Too many requests, please try again later."
+});
+app.use(rateLimiter);
+
+// Routes
 app.use('/api/users', userRoutes);
 app.use('/api/tickets', ticketRoutes);
 app.use('/api/settings', settingRoutes);
 
-
-const rateLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, 
-    max: 50, 
-    message: "Too many requests, please try again later."
-})
-app.use(rateLimiter)
-
-
 app.get('/', (req, res) => {
     res.send('Welcome to the Ticket Management API');
-})
+});
+
+// Error handling middleware
+app.use(errorHandler);
 
 module.exports = app;
