a5071b60117e46c343363b799cdfb7b440828cd7
Babs-unique <145204996+Babs-unique@users.noreply.github.com>
2026-02-27 10:44:06 +0100

    Fixed some errors with image uplaod and cloudinary

--- src/controllers/setting.controller.js
+++ src/controllers/setting.controller.js
@@ -1,4 +1,4 @@
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require('../utils/cloudinary');
 const User = require('../models/user.models');
 const bcrypt = require('bcryptjs')
 
