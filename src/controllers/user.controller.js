c45a6f4e43d6806a0501a2c512238f201ac088ec
Babs-unique <145204996+Babs-unique@users.noreply.github.com>
2026-03-16 20:50:44 +0100

    Temporary fix to unauthorized routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -103,7 +103,7 @@ const refresh = (req, res) => {
                 if (error) {
                     return res.status(401).json({ message: 'Unauthorized' });
                 }
-                const { accessToken } = generateToken(decoded.id);
+                const { accessToken } = generateToken(decoded.userId || decoded.id);
                 res.cookie('accessToken', accessToken, {
                     httpOnly: true,
                     secure: false, //Remember to set this to true in production
@@ -120,12 +120,12 @@ const refresh = (req, res) => {
 }
 const getUserProfile = async (req, res, next) => {
     try {
-        if (!req.user?.id) {
+        if (!req.user?.userId) {
             console.log("User ID from token:", req.user);
             return res.status(401).json({ message: "Unauthorized" });
             
         }
-        const user = await User.findById(req.user.id).select('-password');
+        const user = await User.findById(req.user.userId).select('-password');
         if (!user) {
             const err = new Error('User not found');
             err.status = 404;
--- src/utils/genratedToken.js
+++ src/utils/genratedToken.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 
 
 const generateToken = (userId) => {
-    const accessToken = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '15m' });
-    const refreshToken = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+    const accessToken = jwt.sign({ userId: userId }, process.env.JWT_SECRET, { expiresIn: '15m' });
+    const refreshToken = jwt.sign({ userId: userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
     return { accessToken, refreshToken };
 }
 
