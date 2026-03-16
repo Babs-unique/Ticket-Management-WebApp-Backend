76542c565ac8b95bce7dbe7b07f456dc4f09caaf
Babs-unique <145204996+Babs-unique@users.noreply.github.com>
2026-03-16 20:40:47 +0100

    Temporary fix check

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,11 +3,13 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
     console.log("Auth middleware called");
+    console.log("All cookies:", req.cookies);
     const authHeader = req.cookies?.accessToken; 
    /*  if(!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(401).json({ message: 'Unauthorized' });
     } */
     if(!authHeader) {
+        console.log("No accessToken cookie found");
         return res.status(401).json({ message: 'Unauthorized' });
     }
     try{
@@ -19,7 +21,7 @@ const authMiddleware = (req, res, next) => {
     req.user = decoded;
     next();
     }catch(error){
-        console.error("Authentication error:", error);
+        console.error("Authentication error:", error.message);
         return res.status(401).json({ message: 'Unauthorized' });
     }
 }
